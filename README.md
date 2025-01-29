3f4ba08ff81dd5d247624aafbcf64c9656b44819
Dylan Starink <dylan@starink.com>
2025-01-28 17:54:27 -0800

    Add sample BAML code

---
+++ README.md
@@ -0,0 +1,16 @@
+# SemanticGraphLLM
+
+A CSC 581 project
+
+## Installation for Development
+1. Install [BAML VSCode/Cursor Extension](https://marketplace.visualstudio.com/items?itemName=boundary.baml-extension)
+    
+2. Setup Virtual Environment
+    ```bash
+    python -m venv .venv
+    ```
+
+3. 
+    
+
+
---
+++ main.py
@@ -0,0 +1,32 @@
+from baml_client.sync_client import b
+
+from baml_client.types import Triple
+
+def example(raw_resume: str) -> list[Triple]: 
+
+  # BAML's internal parser guarantees ExtractResume
+
+  # to be always return a Resume type
+
+  response = b.ExtractTriples(raw_resume)
+
+  return response
+
+def example_stream(raw_resume: str) -> list[Triple]:
+
+  stream = b.stream.ExtractTriples(raw_resume)
+
+#   for msg in stream:
+
+#     print(msg) # This will be a PartialResume type
+
+  
+
+  # This will be a Resume type
+
+  final = stream.get_final_response()
+
+  return final
+
+
+print(example_stream("I am a software engineer"))

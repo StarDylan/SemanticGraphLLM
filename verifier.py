a755bbdce32f7a28c7a80fbaec15a9c9bb233e52
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2025-02-18 16:39:01 -0800

    add main to verifier.py

--- verifier.py
+++ verifier.py
@@ -1,5 +1,6 @@
-from owlready2 import *
+from owlready2 import World, sync_reasoner_pellet, OwlReadyInconsistentOntologyError
 from pathlib import Path
+
 
 def check_consistency(ontology_rdf_file: Path, llm_generated_rdf_file: Path):
     # Create a new world to load both the ontology and KG
@@ -13,10 +14,14 @@ def check_consistency(ontology_rdf_file: Path, llm_generated_rdf_file: Path):
         sync_reasoner_pellet(my_world, debug=2)
 
     except OwlReadyInconsistentOntologyError as e:
-        
+
         return False, str(e).split("This is the output of `pellet explain`: \n ")[1]
 
     return True, "KG is consistent with ontology"
 
+
 if __name__ == "__main__":
-    print(f"\n\n\n{check_consistency(Path('ontologies/pizza.example.rdf'), Path('ontologies/pizza_invalid.rdf'))[1]}")
+    print(
+        f"\n\n\n{check_consistency(Path('ontologies/pizza_ontology.owl'), Path('ontologies/pizza_invalid.rdf'))[1]}"
+    )
+
